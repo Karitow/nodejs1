899e4832746212f315ec4c74cd8ec379f6ef95bc
karitow <algo,delicado@gmail.com>
2024-07-10 20:35:24 -0400

    linea comentario

--- operaciones.js
+++ operaciones.js
@@ -1,12 +1,14 @@
 const fs = require ('fs')
 
 const registrar = (Nombre, edad, tipo, color, enfermedad) => {
+    //analiza una cadena de texto como json fs.rfy es una funcion que lee el archivo de forma sincrona
     const citas = JSON.parse(fs.readFileSync('citas.json', 'utf8'));
     if (!Nombre || !edad || !tipo || !color ||!enfermedad){
         console.log('Faltan datos para registrar tu cita');
         return;
     }
     citas.push({Nombre, edad, tipo, color, enfermedad});
+    // \\fswfs escribe datos de forma sincrona y bloquea buble de eventos hasta completar la escritura del archivo - stringify funcion que convierte obj js en cadenas con formato json
     fs.writeFileSync('citas.json', JSON.stringify(citas));
     console.log(citas)
 };
